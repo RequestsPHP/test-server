801d0454748af555944446ade9082d9d0021a2e8
Alain Schlesser <alain.schlesser@gmail.com>
2023-06-26 10:31:07 +0200

    Move ports to 80

--- bin/start.sh
+++ bin/start.sh
@@ -6,10 +6,11 @@ set -x
 # Note: The value of $0 has changed with Composer 2.2, so we're relying on
 # Composer 2.2+ as the required minimum now.
 SERVERDIR="$(dirname $0)"
-PORT=${PORT:-"8080"}
+PORT=${PORT:-"80"}
 
 PHPBIN=${PHPBIN:-"$(which php)"}
 SERVERADDRESS="-S 0.0.0.0:$PORT"
 ARGS="$SERVERADDRESS $SERVERDIR/serve.php"
+PIDFILE="$SERVERDIR/http.pid"
 
-$PHPBIN -- $ARGS
+start-stop-daemon --start --background --pidfile $PIDFILE --make-pidfile --exec $PHPBIN -- $ARGS
